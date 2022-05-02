b104f6b8383059b7ade7ab3f1cccdbfb47d48e45
bigbenster702 <48159706+bigbenster702@users.noreply.github.com>
2022-05-02 03:51:29 +0000

    swag

---
+++ commands/exit.js
@@ -0,0 +1,31 @@
+const Discord = require('discord.js');
+
+module.exports = {
+    name: 'exit',
+    description: 'Creates the exit car meme.',
+  	aliases: ['exit'],
+  	usage: '<car> <left sign> <right sign>',
+	  category: 'Image',
+async execute(client, message, args, Discord){
+    const memetext1 = args[0];
+    if (!nothing) {
+      return message.channel.send("Enter the text to be placed on the car!");
+    }
+    const memetext2 = args[1];
+    if (!memetext1) {
+      return message.channel.send("Enter the text to be placed at left side of the sign!");
+    }
+    const memetext3 = args[2];
+    if (!memetext2) {
+      return message.channel.send("Enter the text to be placed at the right side of the sign!");
+    }
+    message.channel.send({
+      files: [
+        {
+          attachment: `https://api.memegen.link/images/exit/${memetext1}/${memetext2}/${memetext3}`,
+          name: "exitmeme.png",
+        },
+      ],
+    });
+	}
+}
