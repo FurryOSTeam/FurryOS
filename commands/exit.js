f557d5a108f2328443adb52bdced2f1f399c5851
bigbenster702 <48159706+bigbenster702@users.noreply.github.com>
2022-05-02 03:56:16 +0000

    OK

--- commands/exit.js
+++ commands/exit.js
@@ -4,20 +4,20 @@ module.exports = {
     name: 'exit',
     description: 'Creates the exit car meme.',
   	aliases: ['exit'],
-  	usage: '<car> <left sign> <right sign>',
+  	usage: '<left sign> <right sign> <car>',
 	  category: 'Image',
 async execute(client, message, args, Discord){
     const memetext1 = args[0];
     if (!memetext1) {
-      return message.channel.send("Enter the text to be placed on the car!");
+      return message.channel.send("Enter the text to be placed at left side of the sign!");
     }
     const memetext2 = args[1];
     if (!memetext2) {
-      return message.channel.send("Enter the text to be placed at left side of the sign!");
+      return message.channel.send("Enter the text to be placed at right side of the sign!");
     }
     const memetext3 = args[2];
     if (!memetext3) {
-      return message.channel.send("Enter the text to be placed at the right side of the sign!");
+      return message.channel.send("Enter the text to be placed on the car!");
     }
     message.channel.send({
       files: [
