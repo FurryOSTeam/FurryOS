5d6ddbd75cab3d1968f2145033e8cafd2efaf278
bigbenster702 <48159706+bigbenster702@users.noreply.github.com>
2022-05-21 22:45:52 +0000

    I GOT THE REST DONE WOOOOO

---
+++ commands/Moderation/delwarn.js
@@ -0,0 +1,50 @@
+const mongo = require('../../handlers/mongo')
+const warnSchema = require('../../handlers/warnSchema')
+
+const Discord = require('discord.js');
+
+module.exports = {
+    name: "delwarn",
+    category: "Moderation",
+    description: "Deletes a warn from a user.",
+    ownerOnly: false,
+    options: [
+      {
+          name: "user",
+          description: "User with the warn.",
+          type: 'USER',
+          required: true
+      },
+      {
+          name: "id",
+          description: "Id of the warn.",
+          type: 'STRING',
+          required: true
+      }
+  ],
+    run: async (client, interaction) => {
+    const guild = interaction.guild.id
+    if(!interaction.member.permissions.has("KICK_MEMBERS")) {
+      await interaction.reply({ content: "You do not have permission to delete someones warn! You need kick members on your role!", ephemeral: true }); return
+    }
+
+      await mongo().then(async (mongoose) => {
+        try{
+           await warnSchema.findOneAndUpdate({ guildId: guild }, { $pull: { "warnings": { "warnID": interaction.options.getString("id") } }}, { safe: true, multi:true }, function(err, obj) {})
+         await interaction.reply({ embeds: [new Discord.MessageEmbed()
+            .setColor('BLUE')
+            .setTitle(`✅ Deleted warning!`)
+            .setTimestamp()
+            .setFooter({ text: 'FurryOS'})]})
+          mongoose.connection.close()
+        }catch(err){
+          await interaction.reply({ embeds: [new Discord.MessageEmbed()
+            .setColor('BLUE')
+            .setTitle(`❌ This warning does not exist!`)
+            .setTimestamp()
+            .setFooter({ text: 'FurryOS'})]})
+          console.log(err)
+          }
+       })
+    }
+}
---
+++ commands/Moderation/warnings.js
@@ -0,0 +1,61 @@
+const mongo = require('../../handlers/mongo')
+const warnSchema = require('../../handlers/warnSchema')
+
+const Discord = require('discord.js');
+
+module.exports = {
+    name: "warnings",
+    category: "Moderation",
+    description: "Shows warnings for a user.",
+    ownerOnly: false,
+    options: [
+      {
+          name: "user",
+          description: "User to show the warnings of.",
+          type: 'USER',
+          required: true
+      }
+  ],
+    run: async (client, interaction) => {
+    const target = interaction.options.getUser("user");
+    if(!interaction.member.permissions.has("KICK_MEMBERS")) {
+      await interaction.reply({ content: "You do not have permission to see someones warnings! You need kick members on your role!", ephemeral: true }); return
+    }
+
+    const guildId = interaction.guild.id
+    const userId = target.id
+
+    await mongo().then(async (mongoose) => {
+      try {
+        const isempty = await warnSchema.findOne({ "warnings": { $exists: true, $not: {$size: 0} } });
+        const results = await warnSchema.findOne({
+          guildId,
+          userId,
+        })
+        if(!results || isempty === null) return await interaction.reply({ embeds: [new Discord.MessageEmbed()
+        .setColor('BLUE')
+        .setTitle(`No warnings found`)
+        .setTimestamp()
+        .setFooter({ text: 'FurryOS' })]})
+       
+        let reply = new Discord.MessageEmbed()
+        .setColor('BLUE')
+        .setTitle(`Warnings for: ${userId}`)
+        .setTimestamp()
+        .setFooter({ text: 'FurryOS' })
+        
+        for (const warning of results.warnings) {
+          const { author, timestamp, reason, warnID } = warning
+
+          reply .addFields({ name: `Warning - ${warnID}`, value: `By ${author} on ${new Date(
+            timestamp
+          ).toLocaleDateString()} for "${reason}" \n\n`})
+        }
+
+        await interaction.reply({ embeds: [reply] })
+      } finally {
+        mongoose.connection.close()
+      }
+    })
+  },
+}
