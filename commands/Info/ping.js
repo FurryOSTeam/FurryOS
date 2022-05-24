e5f049fca763809894a129e31f0e706cfd3b8395
bigbenster702 <48159706+bigbenster702@users.noreply.github.com>
2022-05-24 14:17:21 -0500

    nvm :(

--- commands/Info/ping.js
+++ commands/Info/ping.js
@@ -1,5 +1,4 @@
 const { MessageEmbed } = require('discord.js');
-const mongoose = require('mongoose');
 
 module.exports = {
     name: "ping",
@@ -14,18 +13,10 @@ module.exports = {
     const timestamp = (interaction.editedTimestamp) ? interaction.editedTimestamp : interaction.createdTimestamp;
     const latency = `\`\`\`ini\n[ ${Math.floor(msg.createdTimestamp - timestamp)}ms ]\`\`\``;
     const apiLatency = `\`\`\`ini\n[ ${Math.round(interaction.client.ws.ping)}ms ]\`\`\``;
-
-    const currentNano = process.hrtime();
-		await mongoose.connection.db.command({ ping: 1 });
-		const time = process.hrtime(currentNano);
-		const mongolat = (time[0] * 1e9 + time[1]) * 1e-6;
-    const mongoLatency = `\`\`\`ini\n[ ${Math.round(mongolat)}ms ]\`\`\``;
-
     embed.setTitle(`Pong! 🏓`)
       .setDescription('')
       .addField('Latency', latency, true)
       .addField('API Latency', apiLatency, true)
-      .addField('Mongo Latency', mongoLatency, true)
       .setTimestamp();
     msg.edit({ embeds: [embed] });
   }
