34ce8bdf28cb2c2f7cf551706c093cfd7b8f981c
bigbenster702 <48159706+bigbenster702@users.noreply.github.com>
2022-05-25 13:03:53 -0500

    avatar stuff wooo

--- commands/Fun/avatar.js
+++ commands/Fun/avatar.js
@@ -11,17 +11,41 @@ module.exports = {
             description: 'User to get their profile picture of.',
             type: 'USER',
             required: true
-        }
+        },
+        {
+          name: 'server',
+          description: 'Gets the users server avatar.',
+          type: 'SUB_COMMAND',
+          required: false
+        },
+        {
+          name: 'global',
+          description: 'Gets the users global avatar.',
+          type: 'SUB_COMMAND',
+          required: false
+      }
     ],
     run: async (client, interaction) => {
     const user = interaction.options.getUser("user")
-
-    const embed = new Discord.MessageEmbed()
-      .setTitle(`${user.username}'s Avatar`)
-      .setColor('BLUE')
-      .setImage(user.displayAvatarURL({ dynamic: true, size: 1024 }))
-      .setDescription(`[Png](${user.avatarURL({ format: 'png' })}) | [Webp](${user.avatarURL({ dynamic: true })}) | [Jpg](${user.avatarURL({ format: 'jpg' })})`)
-      .setFooter({ text: `Requested by: ${interaction.user.username}`, iconURL: interaction.user.displayAvatarURL({ dynamic: true }) });
-    await interaction.reply({ embeds: [embed] });
+    const choice = interaction.options.getSubcommand()
+    
+    if(choice === "global") {
+      const embed = new Discord.MessageEmbed()
+        .setTitle(`${user.username}'s Avatar`)
+        .setColor('BLUE')
+        //.setImage(user.displayAvatarURL({ dynamic: true, size: 1024 }))
+        .setImage(user.displayAvatarURL())
+        .setDescription(`[Png](${user.avatarURL({ format: 'png' })}) | [Webp](${user.avatarURL({ dynamic: true })}) | [Jpg](${user.avatarURL({ format: 'jpg' })})`)
+        .setFooter({ text: `Requested by: ${interaction.user.username}`, iconURL: interaction.user.displayAvatarURL({ dynamic: true }) });
+      await interaction.reply({ embeds: [embed] });
+    } else if(choice === "server") {
+      const embed = new Discord.MessageEmbed()
+        .setTitle(`${user.username}'s Avatar`)
+        .setColor('BLUE')
+        .setImage(user.displayAvatarURL({ dynamic: true, size: 1024 }))
+        .setDescription(`[Png](${user.avatarURL({ format: 'png' })}) | [Webp](${user.avatarURL({ dynamic: true })}) | [Jpg](${user.avatarURL({ format: 'jpg' })})`)
+        .setFooter({ text: `Requested by: ${interaction.user.username}`, iconURL: interaction.user.displayAvatarURL({ dynamic: true }) });
+      await interaction.reply({ embeds: [embed] });
+    }
   }
 }
