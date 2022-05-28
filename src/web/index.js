e049db635deeea0e2abe43876070947d977f8f2c
bigbenster702 <48159706+bigbenster702@users.noreply.github.com>
2022-05-28 00:18:07 -0500

    what

--- src/web/index.js
+++ src/web/index.js
@@ -4,6 +4,7 @@ const app = express();
 app.get('/', (request, response) => {
     return response.sendFile('./src/web/index.html', { root: '.' });
 });
+app.use(express.static(path.join(__dirname, './src/web/index.html')));
 
 const listener = app.listen(process.env.PORT, () => {
     console.log('Your app is currently listening on port: ' + listener.address().port);
