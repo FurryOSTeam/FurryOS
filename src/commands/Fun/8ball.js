4bd3f2122d98f6edb93a4ef6a5ee4b11150a63fb
bigbenster702 <48159706+bigbenster702@users.noreply.github.com>
2022-05-28 15:32:05 -0500

    ok

--- src/commands/Fun/8ball.js
+++ src/commands/Fun/8ball.js
@@ -15,18 +15,15 @@ module.exports = {
       }
   ],
     run: async (client, interaction) => {
-  
-		  let question = interaction.options.getString("question");
-
-		  let replies = ["Yes.", "No.", "Ask again later.", "Maybe.", "Yes and definitely.", "It is certain.", "As I see it, yes.", "Very doubtful.", "Eh I will say yes to that.", "NO!", "Never.", "Nope."];
+  	  let replies = ["Yes.", "No.", "Ask again later.", "Maybe.", "Yes and definitely.", "It is certain.", "As I see it, yes.", "Very doubtful.", "Eh I will say yes to that.", "NO!", "Never.", "Nope."];
 
 		  let result = Math.floor((Math.random() * replies.length));
 
 		  let ballembed = new Discord.MessageEmbed()
 		  	.setAuthor({ name: interaction.user.tag})
 		  	.setColor(client.config.embedcolors.default)
-		  	.addField({ name: 'Question', value: `${question}` })
-		  	.addField({ name: 'Answer', value: `${replies[result]}` })
+		  	.addField({ name: "Question", value: interaction.options.getString("question") })
+		  	.addField({ name: "Answer", value: replies[result] })
         .setTimestamp()
         .setFooter({ text: client.config.embedfooterText, iconURL: client.user.displayAvatarURL({ dynamic: true }) });
 
