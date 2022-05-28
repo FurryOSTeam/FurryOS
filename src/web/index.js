dcae4af284e53247cda326e1c866058c224279f8
bigbenster702 <48159706+bigbenster702@users.noreply.github.com>
2022-05-28 00:44:51 -0500

    asfdsfdfda

--- src/web/index.js
+++ src/web/index.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
 
+app.use(express.static("/"));
+app.use('/css', express.static(__dirname + '/css'));
+app.use('/img', express.static(__dirname + '/img'));
+app.use('/js', express.static(__dirname + '/js'));
+
 app.get('/', (request, response) => {
     return response.sendFile('./src/web/index.html', { root: '.' });
 });
