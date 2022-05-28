a0073992c5bf663f150b0803779e732632e95f51
bigbenster702 <48159706+bigbenster702@users.noreply.github.com>
2022-05-28 15:46:11 -0500

    fine i am done

--- src/commands/Fun/8ball.js
+++ src/commands/Fun/8ball.js
@@ -15,19 +15,18 @@ module.exports = {
       }
   ],
     run: async (client, interaction) => {
-  	  let replies = ["Yes.", "No.", "Ask again later.", "Maybe.", "Yes and definitely.", "It is certain.", "As I see it, yes.", "Very doubtful.", "Eh I will say yes to that.", "NO!", "Never.", "Nope."];
+  
+		  let question = interaction.options.getString("question");
+
+		  let replies = ["Yes.", "No.", "Ask again later.", "Maybe.", "Yes and definitely.", "It is certain.", "As I see it, yes.", "Very doubtful.", "Eh I will say yes to that.", "NO!", "Never.", "Nope."];
 
 		  let result = Math.floor((Math.random() * replies.length));
 
 		  let ballembed = new Discord.MessageEmbed()
 		  	.setAuthor({ name: interaction.user.tag})
 		  	.setColor(client.config.embedcolors.default)
-		  	.addField(
-            { name: "Question", value: interaction.options.getString("question") },
-            { name: "Answer", value: replies[result] }
-        )
-        .setTimestamp()
-        .setFooter({ text: client.config.embedfooterText, iconURL: client.user.displayAvatarURL({ dynamic: true }) });
+		  	.addField("Question", question)
+		  	.addField("Answer", replies[result]);
 
 		  await interaction.reply({ embeds: [ballembed] })
 	}
