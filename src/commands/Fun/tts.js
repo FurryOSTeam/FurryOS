0e0d448c9db545ff8bbd072b9fe78568f6ee2a70
bigbenster702 <48159706+bigbenster702@users.noreply.github.com>
2022-06-30 14:18:00 -0500

    maybe?

--- src/commands/Fun/tts.js
+++ src/commands/Fun/tts.js
@@ -15,7 +15,7 @@ module.exports = {
       }
   ],
     run: async (client, interaction) => {
-    let ttsmessage = interaction.options.getString("message").replace(/[^\w\s]|_/g,"");
+    let ttsmessage = interaction.options.getString("message") //.replace(/[^\w\s]|_/g,"");
     if (ttsmessage.length > 1024) return interaction.reply({ content: '**You Are Not Allowed To Go Over 1024 Characters!**', ephemeral: true });
 
     await interaction.reply({ files: [{ attachment: `https://tts.cyzon.us/tts?text=${ttsmessage}`, name: 'tts.mp3' }] });
