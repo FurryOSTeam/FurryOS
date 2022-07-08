4d6063caec6185329832d0332e7715aef313a67d
bigbenster702 <48159706+bigbenster702@users.noreply.github.com>
2022-07-08 13:25:48 -0500

    yipee

---
+++ src/commands/Info/poll.js
@@ -0,0 +1,29 @@
+const { MessageEmbed } = require('discord.js');
+
+module.exports = {
+    name: "poll",
+    usage: "/poll",
+    category: "Info",
+    description: "Creates a poll.",
+    ownerOnly: false,
+    options: [
+        {
+            name: "question",
+            description: "The question of the poll.",
+            type: 'STRING',
+            required: true
+        }
+    ],
+    run: async (client, interaction) => {
+        const question = interaction.options.getString("question");
+
+        const embed = new MessageEmbed()
+        .setTitle("📊 Poll")
+        .setDescription(question);
+        
+        await message.channel.send({ embeds: [embed] }).then(msg => {
+            msg.react('👍');
+            msg.react('👎');
+        });
+  }
+}
