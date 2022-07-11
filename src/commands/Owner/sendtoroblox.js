e3d734210201711d819582657dd58a2301d35b16
bigbenster702 <48159706+bigbenster702@users.noreply.github.com>
2022-07-11 14:29:40 -0500

    test

---
+++ src/commands/Owner/sendtoroblox.js
@@ -0,0 +1,49 @@
+const Discord = require('discord.js');
+const fetch = require('axios');
+
+module.exports = {
+    name: "sendtoroblox",
+    usage: "/sendtoroblox <user>",
+    category: "Owner",
+    description: "Sends a message to a roblox game.",
+    ownerOnly: true,
+    options: [
+        {
+            name: 'message',
+            description: 'Message to send.',
+            type: 'STRING',
+            required: true
+        }
+    ],
+    run: async (client, interaction) => {
+    const mess = interaction.options.getString("message");
+
+    try {
+        let res = await fetch.post(`https://apis.roblox.com/messaging-service/v1/universes/1725206480/topics/Announcements`, {
+            headers: {
+                "x-api-key": process.env.thingy
+            },
+            data: {
+                message: mess,
+            }
+        });
+        const embed = new MessageEmbed()
+            .setColor(client.config.embedcolors.success)
+            .setTitle(`Message.`)
+            .addField('Successfully to sent message: ', `${mess}`, true)
+            .setTimestamp()
+            .setFooter({ text: client.config.embedfooterText, iconURL: client.user.displayAvatarURL({ dynamic: true }) });
+
+        await interaction.reply({ embeds: [embed] });
+    } catch (err) {
+        const embed = new MessageEmbed()
+            .setColor(client.config.embedcolors.error)
+            .setTitle(`Message.`)
+            .addField('Failed to send message: ', `${mess}`, true)
+            .setTimestamp()
+            .setFooter({ text: client.config.embedfooterText, iconURL: client.user.displayAvatarURL({ dynamic: true }) });
+
+        await interaction.reply({ embeds: [embed] });
+    }
+  }
+}
