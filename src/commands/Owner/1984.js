5a2408aecfb70d822cc6dcc316f4e57cc67d3f90
bigbenster702 <48159706+bigbenster702@users.noreply.github.com>
2022-07-22 17:24:56 -0500

    m

--- src/commands/Owner/1984.js
+++ src/commands/Owner/1984.js
@@ -19,19 +19,30 @@ module.exports = {
         }
     ],
     run: async (client, interaction) => {
-        if (ids.id.includes(interaction.user.id)) {
-            const role = interaction.guild.roles.cache.find(r => r.id === "999868292162404464");
-            const member = interaction.options.getUser("user");
-            member.roles.add(role);
-            const embed = new Discord.MessageEmbed()
-                .setTitle("1984'd")
-                .setFooter({ text: client.config.embedfooterText, iconURL: client.user.displayAvatarURL({ dynamic: true }) })
-                .setTimestamp()
-                .setDescription(`${member.user.tag} has been 1984'd.`)
-                .setColor(client.config.embedcolors.default)
-            await interaction.reply({ embeds: [embed] });
-        } else {
-            await interaction.reply({ content: "You don't have permission to use this command.", ephemeral: true })
+        try {
+            if (ids.id.includes(interaction.user.id)) {
+                const member = interaction.options.getUser("user");
+                interaction.member.roles.add("999868292162404464");
+                const embed = new Discord.MessageEmbed()
+                    .setTitle("1984'd")
+                    .setFooter({ text: client.config.embedfooterText, iconURL: client.user.displayAvatarURL({ dynamic: true }) })
+                    .setTimestamp()
+                    .setDescription(`${member.user.tag} has been 1984'd.`)
+                    .setColor(client.config.embedcolors.default)
+                await interaction.reply({ embeds: [embed] });
+            } else {
+                await interaction.reply({ content: "You don't have permission to use this command.", ephemeral: true })
+            }
+        } catch (err) {
+        const embed = new Discord.MessageEmbed()
+            .setColor(client.config.embedcolors.error)
+            .setTitle(`Error.`)
+            .addField('Failed to give role to: ', `${member.user.tag}`)
+            .addField('Error: ', `${err}`)
+            .setTimestamp()
+            .setFooter({ text: client.config.embedfooterText, iconURL: client.user.displayAvatarURL({ dynamic: true }) });
+
+        await interaction.reply({ embeds: [embed] });
         }
 	}
 }
