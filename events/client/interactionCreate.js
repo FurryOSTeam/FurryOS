2fdbd8fc85cce463881e2769e76f36e91d0670df
bigbenster702 <48159706+bigbenster702@users.noreply.github.com>
2022-07-23 18:02:44 -0500

    owner fixes

--- events/client/interactionCreate.js
+++ events/client/interactionCreate.js
@@ -19,17 +19,33 @@ client.on('interactionCreate', async interaction => {
 		try {
 			if(slashCommand.cooldown) {
 				if(cooldown.has(`slash-${slashCommand.name}${interaction.user.id}`)) return interaction.reply({ content: config.messages["COOLDOWN_MESSAGE"].replace('<duration>', ms(cooldown.get(`slash-${slashCommand.name}${interaction.user.id}`) - Date.now(), {long : true}) ) })
+				if (slashCommand.ownerOnly) {
+					if (!client.config.ownerID.includes(interaction.user.id)) {
+						return interaction.reply({ embeds: [new Discord.MessageEmbed()
+							.setColor(client.config.embedcolors.error)
+							.setTitle(`Error.`)
+							.setDescription(`This command only for the bot owner!`)
+							.setTimestamp()
+							.setFooter({ text: client.config.embedfooterText})], ephemeral: true });
+					}
+				}
 				if(slashCommand.userPerms || slashCommand.botPerms) {
 					if(!interaction.memberPermissions.has(PermissionsBitField.resolve(slashCommand.userPerms || []))) {
 						const userPerms = new EmbedBuilder()
+							.setTitle(`Error.`)
 							.setDescription(`🚫 ${interaction.user}, You don't have \`${slashCommand.userPerms}\` permissions to use this command!`)
-							.setColor('Red')
+							.setColor(client.config.embedcolors.error)
+							.setTimestamp()
+							.setFooter({ text: client.config.embedfooterText, iconURL: client.user.displayAvatarURL({ dynamic: true }) })
 						return interaction.reply({ embeds: [userPerms] })
 					}
 					if(!interaction.guild.members.cache.get(client.user.id).permissions.has(PermissionsBitField.resolve(slashCommand.botPerms || []))) {
 						const botPerms = new EmbedBuilder()
+							.setTitle(`Error.`)
 							.setDescription(`🚫 ${interaction.user}, I don't have \`${slashCommand.botPerms}\` permissions to use this command!`)
-							.setColor('Red')
+							.setColor(client.config.embedcolors.error)
+							.setTimestamp()
+							.setFooter({ text: client.config.embedfooterText, iconURL: client.user.displayAvatarURL({ dynamic: true }) })
 						return interaction.reply({ embeds: [botPerms] })
 					}
 
@@ -41,17 +57,33 @@ client.on('interactionCreate', async interaction => {
 							cooldown.delete(`slash-${slashCommand.name}${interaction.user.id}`)
 					}, slashCommand.cooldown)
 			} else {
+				if (command.ownerOnly) {
+					if (!client.config.ownerID.includes(interaction.user.id)) {
+						return interaction.reply({ embeds: [new Discord.MessageEmbed()
+							.setColor(client.config.embedcolors.error)
+							.setTitle(`Error.`)
+							.setDescription(`This command only for the bot owner!`)
+							.setTimestamp()
+							.setFooter({ text: client.config.embedfooterText})], ephemeral: true });
+					}
+				}
 				if(slashCommand.userPerms || slashCommand.botPerms) {
 					if(!interaction.memberPermissions.has(PermissionsBitField.resolve(slashCommand.userPerms || []))) {
 						const userPerms = new EmbedBuilder()
+							.setTitle(`Error.`)
 							.setDescription(`🚫 ${interaction.user}, You don't have \`${slashCommand.userPerms}\` permissions to use this command!`)
-							.setColor('Red')
+							.setColor(client.config.embedcolors.error)
+							.setTimestamp()
+							.setFooter({ text: client.config.embedfooterText, iconURL: client.user.displayAvatarURL({ dynamic: true }) })
 						return interaction.reply({ embeds: [userPerms] })
 					}
 					if(!interaction.guild.members.cache.get(client.user.id).permissions.has(PermissionsBitField.resolve(slashCommand.botPerms || []))) {
 						const botPerms = new EmbedBuilder()
+							.setTitle(`Error.`)
 							.setDescription(`🚫 ${interaction.user}, I don't have \`${slashCommand.botPerms}\` permissions to use this command!`)
-							.setColor('Red')
+							.setColor(client.config.embedcolors.error)
+							.setTimestamp()
+							.setFooter({ text: client.config.embedfooterText, iconURL: client.user.displayAvatarURL({ dynamic: true }) })
 						return interaction.reply({ embeds: [botPerms] })
 					}
 
--- handlers/slashCommand.js
+++ handlers/slashCommand.js
@@ -3,14 +3,14 @@ const chalk = require('chalk');
 require('dotenv').config();
 
 const { PermissionsBitField } = require('discord.js');
-const { Routes } = require('discord-api-types/v9');
+const { Routes } = require('discord-api-types/v10');
 const { REST } = require('@discordjs/rest')
 
 const AsciiTable = require('ascii-table');
 const table = new AsciiTable().setHeading('Slash Commands', 'Stats').setBorder('|', '=', "0", "0")
 const TOKEN = process.env.token;
 
-const rest = new REST({ version: '9' }).setToken(TOKEN);
+const rest = new REST({ version: '10' }).setToken(TOKEN);
 
 module.exports = (client) => {
 	const CLIENT_ID = client.config.botClientID;
